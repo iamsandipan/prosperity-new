c1848ab7d783a6c36b88b38b573b4b9fa899d8e6
sandipan.chakrabarti <sandipan.chakrabarti@gmail.com>
2015-09-30 01:14:54 -0700

    Added

--- src/main/java/com/prosperity/dao/MongoAdapter.java
+++ src/main/java/com/prosperity/dao/MongoAdapter.java
@@ -1,5 +1,14 @@
 package com.prosperity.dao;
 
+import com.mongodb.MongoClient;
+
 public class MongoAdapter {
+	
+	public void connectToMongo(){
+		MongoClient mongoClient = new MongoClient();
+		
+		mongoClient.getDatabase("users");
+		
+	}
 
 }
